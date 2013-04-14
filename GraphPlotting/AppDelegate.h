0f503dc9eb456a811c5a07d4e1305d1b41a47b33
Ansh Kumar Khanna <anshkumarkhanna@gmail.com>
2013-04-14 13:28:54 +0530

    Initial Commit

---
+++ GraphPlotting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GraphPlotting
+//
+//  Created by Puneet Sharma on 14/04/13.
+//  Copyright (c) 2013 Puneet Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GraphPlotting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GraphPlotting
+//
+//  Created by Puneet Sharma on 14/04/13.
+//  Copyright (c) 2013 Puneet Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
